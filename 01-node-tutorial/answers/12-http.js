4a36f5ea2821f169670bd67ae2d4ed071a585228
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-03-22 12:11:45 -0500

    http issue fixed

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -1,17 +1,17 @@
 const http = require('http');
 
-const server = http.createServer((req, res)=>{
+const serverTry = http.createServer((req, res)=>{
    if(req.url === '/'){
    res.end('Hello Mariana, this is a server')
    }
    if(req.url === '/about'){
     res.end('this is about')
    }
-  req.end(`<h1>Oooopsssss</h1>
+  resgit .end(`<h1>Oooopsssss</h1>
   <a href='/'>Back home</a>`)
    
     
 
 })
 
-server.listen(4000)
+serverTry.listen(4000)
