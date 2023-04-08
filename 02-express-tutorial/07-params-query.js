19c27a25f1df0307f040db99149a0a7a2668ccb7
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2023-04-08 12:05:00 -0500

    error says singleProduct is not defined

--- 02-express-tutorial/07-params-query.js
+++ 02-express-tutorial/07-params-query.js
@@ -5,7 +5,7 @@ const {products}= require('./data.js')
 app.get('/', (req,res)=>{
     res.send('<h1>Home Page</h1><a href="api/products/products>Products</a>')
 })
-app.get('api/products',(req,res)=>{
+app.get('/api/products',(req,res)=>{
 const newProducts = products.map((product)=>{
     const {id, name, image} = product;
     return {id, name, image}
@@ -22,11 +22,39 @@ const singleProduct = products.find((product)=>product.id === Number(productID))
 if(!singleProduct){
     return res.status(404).send('Product does not exist')
 }
-res.json(singleProduct)
+return res.json(singleProduct)
 
 });
 
+app.get('/api/products/:productID/reviews/reviewID', (req,res)=>{
+    console.log(req.params)
+res.send('Hello world')
+})
+
+app.get('/api/v1/query', (req, res)=>{
+// console.log(req.query)
+const {search, limit} = req.query;
+let sortedProducts = [...products];
+
+if(search){
+    sortedProducts = sortedProducts.filter((product)=>{
+        return product.name.startsWith(search)
+    })
+}
+if(limit){
+    sortedProducts = sortedProducts.slice(0,Number(limit))
+}
+res.status(200).json(sortedProducts)
+// res.send('hello world')
+if(singleProduct.lenght<1){
+    
+    // res.status(200).send('no products match your search')
+    return res.status(200).json({sucess: true, data:[]})
+}
+return res.status(200).json(sortedProducts);
+})
 
 app.listen(5001,()=>{
-    console.log('Server is listeging to por 5000...')
+    console.log('Server is listeging to por 5001...')
 })
+
